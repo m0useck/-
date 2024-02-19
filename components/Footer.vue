01a595e7b9987af0c78bcbac86f2ba5f21dd78c7
m0useck <polinapolina0621@gmail.com>
2024-02-19 18:40:47 +0300

    consultation

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,38 @@
 <template>
-    <footer class="grid-container w-full mt-16 bg-[#028ECE]/10 py-6">
-        <div class="flex items-center max-lg:flex-col xl:gap-32 md:gap-12 gap-8">
-            <div class="">
-                <img class="md:h-[300px] aspect-video rounded-md" src="/img/footer.png" alt="">
-            </div>
-            <div class="flex flex-col gap-8 max-lg:w-full lg:grow">
-                <div class="flex justify-center items-center gap-6">
-                    <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="skill-icons:github-dark"/></NuxtLink>
-                    <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="logos:telegram"/></NuxtLink>
-                    <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="devicon:figma"/></NuxtLink>
+    <footer class="flex flex-col w-full mt-16 bg-gradient-to-tr from-[#0C669C]/90 to-[#3BBAC2]/90  text-white">
+        <div class="grid-container py-6">
+            <div class="flex items-center max-xl:flex-col xl:gap-32 md:gap-12 gap-8">
+                <div class="flex items-center gap-6">
+                    <img class="max-md:hidden" src="/img/footer/footer.png" alt="">
+                    <img class="max-md:hidden" src="/img/footer/footer2.png" alt="">
+                    <img src="/img/footer/footer3.png" alt="">
                 </div>
-                <div class="flex flex-col gap-4">
-                    <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
-                        <NuxtLink class="flex flex-col w-fit transition-all duration-300 hover:text-[#3BBAC2] after:w-0 after:h-px after:bg-[#3BBAC2] hover:after:w-full after:transition-all after:duration-300">О нас</NuxtLink> 
-                        <span class="bg-black rounded-full w-1 h-1"></span>
-                        <NuxtLink class="flex flex-col w-fit transition-all duration-300 hover:text-[#3BBAC2] after:w-0 after:h-px after:bg-[#3BBAC2] hover:after:w-full after:transition-all after:duration-300">Проверка зрения</NuxtLink> 
-                        <span class="bg-black rounded-full w-1 h-1"></span>
-                        <NuxtLink class="flex flex-col w-fit transition-all duration-300 hover:text-[#3BBAC2] after:w-0 after:h-px after:bg-[#3BBAC2] hover:after:w-full after:transition-all after:duration-300">Доставка и оплата</NuxtLink>
+                <div class="flex flex-col gap-8 max-xl:w-full lg:grow">
+                    <div class="flex justify-center items-center gap-6">
+                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="skill-icons:github-dark"/></NuxtLink>
+                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="logos:telegram"/></NuxtLink>
+                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="devicon:figma"/></NuxtLink>
                     </div>
-                    <span class="bg-black rounded-full w-1 h-1 mx-auto md:hidden"></span>
-                    <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
-                        <NuxtLink class="flex flex-col w-fit transition-all duration-300 hover:text-[#3BBAC2] after:w-0 after:h-px after:bg-[#3BBAC2] hover:after:w-full after:transition-all after:duration-300">Оферта</NuxtLink>
-                        <span class="bg-black rounded-full w-1 h-1"></span>
-                        <NuxtLink class="flex flex-col w-fit transition-all duration-300 hover:text-[#3BBAC2] after:w-0 after:h-px after:bg-[#3BBAC2] hover:after:w-full after:transition-all after:duration-300">Политика конфиденциальности</NuxtLink>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
+                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">О нас</NuxtLink> 
+                            <span class="bg-white rounded-full w-1 h-1"></span>
+                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Проверка зрения</NuxtLink> 
+                            <span class="bg-white rounded-full w-1 h-1"></span>
+                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Доставка и оплата</NuxtLink>
+                        </div>
+                        <span class="bg-white rounded-full w-1 h-1 mx-auto md:hidden"></span>
+                        <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
+                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Оферта</NuxtLink>
+                            <span class="bg-white rounded-full w-1 h-1"></span>
+                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Политика конфиденциальности</NuxtLink>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="grid-container w-full bg-black/25 py-2">
+            <p class="text-center mx-auto">© 2023 Samareva™. All Rights Reserved.</p>
         </div>
     </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-    <div class="flex flex-col items-center gap-6">
         <div class="flex items-center justify-center py-4 px-10 w-full rounded-md gap-16 max-lg:flex-col max-lg:gap-8 bg-gradient-to-r from-[#0C669C] to-[#3BBAC2] text-white">
             <NuxtLink>
                 Оправы
@@ -17,41 +16,77 @@
                 Солнцезащитные очки
             </NuxtLink>
         </div>
-            <Swiper :loop="true" :space-between="20" :modules="[SwiperPagination]" :pagination="{ clickable: true }" class="w-full md:h-[calc(100vh/2)] rounded-md">
+        <div class="relative w-full -mt-6 col-span-full grid-container py-10 bg-[#0C669C]">
+            <div class="absolute w-full h-full top-0 bg-[url(/img/hero/bg.png)] bg-bottom left-0 col-span-full"></div>
+            <Swiper :loop="true" :space-between="20" :modules="[SwiperPagination, SwiperAutoplay]" :pagination="{ clickable: true }" :autoplay="{delay:3500}" class="w-full md:h-[calc(100vh/2)] z-[1] rounded-md shadow-[0px_0px_13px_-9px_black] overflow-hidden">
                 <SwiperSlide class="w-full" v-for="n in 10">
                     <NuxtLink class="w-full">
-                        <img class="object-cover aspect-video w-full" src="https://images.wallpaperscraft.ru/image/single/kotenok_malenkij_igrat_1937_1600x900.jpg" alt="">
+                        <img class="object-cover aspect-video w-full " src="/img/hero/3.jpg" alt="">
+                    </NuxtLink>
+                    <NuxtLink class="w-full">
+                        <img class="object-cover aspect-video w-full " src="/img/hero/4.jpg" alt="">
                     </NuxtLink>
                 </SwiperSlide>
             </Swiper>
-    </div>
+        </div>
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 text-white">
-        <div class="flex flex-col bg-[#218CAD] rounded-md px-6 py-4">
+        <div class="flex flex-col bg-[#218CAD] rounded-md px-6 py-6">
             <div class="flex items-center justify-between">
-                <p class="text-2xl">Очки</p>
-                <Icon class="text-6xl mr-6" name="ion:glasses-outline"/>
+                <p class="text-2xl">Большой ассортимент</p>
+                <Icon class="text-5xl mr-6" name="material-symbols:shopping-bag-outline"/>
             </div>
-            <p class="text-md opacity-70">Если вы нуждаетесь в коррекции зрения, мы подберем качественные и безопасные очки или линзы для глаз.</p>
+            <p class="text-md opacity-70">У нас в магазине оптики огромный выбор оправ и линз для всех возрастов и стилей. Найдите идеальные очки, подчеркивающие вашу уникальность!</p>
         </div>
-        <div class="flex flex-col bg-[#197299] rounded-md px-6 py-4">
+        <div class="flex flex-col bg-[#197299] rounded-md px-6 py-6">
             <div class="flex items-center justify-between">
-                <p class="text-2xl">Очки</p>
-                <Icon class="text-6xl mr-6" name="ion:glasses-outline"/>
+                <p class="text-2xl">Гарантия качества</p>
+                <Icon class="text-5xl mr-6" name="material-symbols:google-guarantee"/>
             </div>
-            <p class="text-md opacity-70">Если вы нуждаетесь в коррекции зрения, мы подберем качественные и безопасные очки или линзы для глаз.</p>
+            <p class="text-md opacity-70">Мы гарантируем высокое качество продукции в магазине оптики. Каждая пара очков проходит строгий контроль, чтобы обеспечить надежность и комфорт для ваших глаз.</p>
         </div>
-        <div class="flex flex-col bg-[#218CAD] rounded-md px-6 py-4">
+        <div class="flex flex-col bg-[#218CAD] rounded-md px-6 py-6">
             <div class="flex items-center justify-between">
-                <p class="text-2xl">Очки</p>
-                <Icon class="text-6xl mr-6" name="ion:glasses-outline"/>
+                <p class="text-2xl">Доступные цены</p>
+                <Icon class="text-5xl mr-6" name="solar:sale-bold"/>
             </div>
-            <p class="text-md opacity-70">Если вы нуждаетесь в коррекции зрения, мы подберем качественные и безопасные очки или линзы для глаз.</p>
+            <p class="text-md opacity-70">Отличное качество по доступным ценам! Найдите стильные и комфортные очки по выгодным предложениям в нашем магазине оптики.</p>
         </div>
     </div>
     <div class="flex flex-col gap-6">
         <p class="text-2xl md:text-3xl xl:text-4xl text-center">Хиты продаж</p>
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <Card v-for="card in cards " v-bind="card"></Card>
+        </div>
+    </div>
+    <div class="relative bg-[url(/img/contact/bg.png)] w-full col-span-full py-16 grid-container">
+        <div class="absolute w-full h-full top-0 left-0 bg-gradient-to-br from-[#0C669C] to-[#3BBAC2] opacity-85 col-span-full"></div>
+        <div class="flex py-10 px-4 md:px-6 xl:px-10 rounded-md bg-white gap-6 z-[1] max-md:flex-col">
+            <div class="md:w-[55%]">
+                <Swiper :loop="true" :space-between="20" :modules="[SwiperAutoplay]" :autoplay="{delay:3000}" class="w-full">
+                    <SwiperSlide class="!flex flex-col gap-8 w-full">
+                        <p class="text-2xl">Близорукость</p>
+                        <p class="opacity-70">Близорукость — распространенное заболевание глаз, при котором человек видит хорошо близко, но плохо вдали. Причина - фокусировка света в области перед сетчаткой. Лечится очками, контактными линзами или хирургическими методами. Раннее выявление и правильная коррекция важны для сохранения зрения и комфортной жизни.</p> 
+                    </SwiperSlide>
+                    <SwiperSlide class="!flex flex-col gap-8 w-full">
+                        <p class="text-2xl">Дальнозоркость</p>
+                        <p class="opacity-70">Дальнозоркость  - состояние, когда человек видит хорошо вдали, но плохо близко. Происходит из-за того, что фокус света падает позади сетчатки. Нередко возникает у детей и может быть унаследовано. Лечится очками, линзами или хирургическим вмешательством. Важна регулярная проверка зрения у офтальмолога для своевременной коррекции и поддержания зрительного комфорта.</p> 
+                    </SwiperSlide>
+                    <SwiperSlide class="!flex flex-col gap-8 w-full">
+                        <p class="text-2xl">Астигматизм</p>
+                        <p class="opacity-70">Астигматизм - это нарушение зрения, когда корневая поверхность глаза не идеально округлая, что вызывает искажение изображения. Человек видит размыто как на близком, так и на дальнем расстоянии. Лечится очками, линзами или хирургически. Важно регулярно проверять зрение у офтальмолога для подбора оптимального способа коррекции и поддержания зрительного комфорта.</p> 
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+            <div class="flex flex-col md:w-[45%] rounded-lg p-1 bg-gradient-to-br from-[#0C669C] to-[#3BBAC2]">
+                <div class="flex flex-col items-center gap-8 bg-white w-full h-full rounded-md px-4 py-6">
+                    <p class="text-center text-xl">Консультация</p>
+                    <div class="flex flex-col gap-4 w-full items-center">
+                        <input class="border border-[#0C669C] px-4 py-1 w-4/5 focus:outline-none focus:ring-0 focus:appearance-none rounded-md" placeholder="Имя" type="text">
+                        <input class="border border-[#0C669C] px-4 py-1 w-4/5 focus:outline-none focus:ring-0 focus:appearance-none rounded-md" placeholder="Телефон" type="text">
+                    </div>
+                    <button class="bg-gradient-to-br from-[#0C669C] to-[#3BBAC2] rounded-md text-white text-center w-1/2 py-2">Звонок</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -59,24 +94,19 @@
 <script setup>
 const cards=[
     {
-        image: "https://images-ext-2.discordapp.net/external/t5MQMWWZ874YFd0Obx4tPmq7VG7ISdr7V5MZQ6qnCAs/https/polotnos.cdnbro.com/posts/9208918-krutye-ochki-risunok-13.jpg?format=webp&width=676&height=676",
-        title: "Очки из майнкрафта для самых крутых и отважных",
-        price: 100000
+        image: "https://www.optic-city.ru/image/cache/rss/1/9/5/3/1/5/9/item_1953159/1953159_0-810x540.jpg.webp",
+        title: "Очки Boss 1428 0OC",
+        price: 14599
     },
     {
-        image: "https://images-ext-2.discordapp.net/external/t5MQMWWZ874YFd0Obx4tPmq7VG7ISdr7V5MZQ6qnCAs/https/polotnos.cdnbro.com/posts/9208918-krutye-ochki-risunok-13.jpg?format=webp&width=676&height=676",
-        title: "Очки из майнкрафта для самых крутых и отважных",
-        price: 100000
+        image: "https://www.optic-city.ru/image/cache/rss/1/8/1/7/8/3/5/item_1817835/1817835_0-810x540.jpg.webp",
+        title: "Очки RayBan 5228 5014 (55)",
+        price: 15749
     },
     {
-        image: "https://images-ext-2.discordapp.net/external/t5MQMWWZ874YFd0Obx4tPmq7VG7ISdr7V5MZQ6qnCAs/https/polotnos.cdnbro.com/posts/9208918-krutye-ochki-risunok-13.jpg?format=webp&width=676&height=676",
-        title: "Очки из майнкрафта для самых крутых и отважных",
-        price: 100000
-    },
-    {
-        image: "https://images-ext-2.discordapp.net/external/t5MQMWWZ874YFd0Obx4tPmq7VG7ISdr7V5MZQ6qnCAs/https/polotnos.cdnbro.com/posts/9208918-krutye-ochki-risunok-13.jpg?format=webp&width=676&height=676",
-        title: "Очки из майнкрафта для самых крутых и отважных",
-        price: 100000
+        image: "https://www.optic-city.ru/image/cache/rss/1/9/1/3/4/7/1/item_1913471/1913471_0-810x540.jpg.webp",
+        title: "Очки PRODESIGN 1762 1 9631",
+        price: 199899
     }
 ]
 </script>
